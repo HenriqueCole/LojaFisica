8f53b1c0ffc3cd18318f88448945abc2576c79c1
HenriqueCole <henriquecolefernandes@gmail.com>
2022-03-04 15:31:17 -0300

    Testee

---
+++ src/Pessoa.java
@@ -0,0 +1,62 @@
+public class Pessoa extends Endereco{
+
+    private String nome;
+    private String cpf;
+    private int idade;
+    private String email;
+    private String telefone;
+    private String genero;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void comprar(){
+
+    }
+
+}
